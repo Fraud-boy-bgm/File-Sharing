c8d45e7a06fbf48d878ccb1bd95cdbae391ea269
Madhu D <92590082+Fraud-boy-bgm@users.noreply.github.com>
2022-03-27 12:28:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -107,8 +107,8 @@ async def _main(bot, update):
         return
     me = await bot.get_me()
     await update.reply_text(
-        'Here is Your Sharing Link:', 
-        True,
+        'Here is Your Sharing Link:'https://t.me/{me.username}?start={unique_idx.lower()}-{str(msg_id)},
+            True,
         reply_markup=InlineKeyboardMarkup([
             [InlineKeyboardButton('Sharing Link', url=f'https://t.me/{me.username}?start={unique_idx.lower()}-{str(msg_id)}')]
         ]) 
